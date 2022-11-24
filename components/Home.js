ccbef52f052d0fc91bc40dabc7bd89e6f8108e16
Elmaleh Tom <tomelmaleh@hotmail.com>
2022-11-24 12:07:04 +0100

    left section

--- components/Home.js
+++ components/Home.js
@@ -1,13 +1,42 @@
+import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 
 function Home() {
   return (
-    <div>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-      </main>
+    <div className={styles.container}>
+
+      <div className={styles.leftSection}>
+        <div className={styles.logo}>
+        <Image src="/bird-logo-left-section.png"  
+          alt="Logo"
+          width={90}
+          height={90}
+        />
+        </div>
+        <div className={styles.userInfo}>
+          <div className={styles.userPhoto}>
+          <Image src="/bird-logo-left-section.png"  
+            alt="Logo"
+            width={70}
+            height={70}
+          />
+          </div>
+          <div className={styles.userName}>
+            <p>John</p>
+            <p>@JohnCena</p>
+          </div>
+        </div>
+      </div>
+
+      <div className={styles.topSection}>
+        TOP SECTION
+      </div>
+      <div className={styles.tweetSection}>
+        TWEET SECTION
+      </div>  
+      <div className={styles.trendsSection}>
+        TRENDS SECTION
+      </div>
     </div>
   );
 }
